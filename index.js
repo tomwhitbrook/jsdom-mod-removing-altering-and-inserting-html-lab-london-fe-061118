1dacdfb574d7a1ec63ff72a32ce8c70fc2e75f84
Tom Whitbrook <tomwhitbrook@gmail.com>
2018-08-14 17:47:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
+main.remove()
+
+var element = document.createElement("main")
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+const newHeader = document.createElement("h1");
+newHeader.innerText = "your name is the champion!";
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
